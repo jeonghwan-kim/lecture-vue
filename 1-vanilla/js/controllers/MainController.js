b6a4921dbb3cfa1fee7328f3ca07ea05293f06a8
김정환 <ej88ej@gmail.com>
2017-12-23 00:07:16 +0900

    [1-vanilla] FormView 추가

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,7 +1,19 @@
+import FormView from '../views/FormView.js'
+
 const tag = '[MainController]'
 
 export default {
   init() {
-    console.log(tag, 'init()')
+    FormView.setup(document.querySelector('form'))
+      .on('@submit', e => this.onSubmit(e.detail.input))
+      .on('@reset', e => this.onResetForm())
+  },
+
+  onSubmit(input) {
+    console.log(tag, 'onSubmit()', input)
+  },
+  
+  onResetForm() {
+    console.log(tag, 'onResetForm()')
   }
 }
