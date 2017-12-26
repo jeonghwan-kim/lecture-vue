26de5beada9eb09da13a39d37d5467032bbe562b
김정환 <ej88ej@gmail.com>
2017-12-26 20:35:08 +0900

    [1-vanilla] ResultView 추가

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
 
@@ -7,13 +10,27 @@ export default {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
       .on('@reset', e => this.onResetForm())
+
+    ResultView.setup(document.querySelector('#search-result'))
   },
 
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
+    this.search(input)
   },
   
   onResetForm() {
     console.log(tag, 'onResetForm()')
+    ResultView.hide()
+  },
+
+  search(query) {
+    SearchModel.list(query).then(data => {
+      this.onSearchResult(data)
+    })
+  },
+
+  onSearchResult(data) {
+    ResultView.render(data)
   }
 }
