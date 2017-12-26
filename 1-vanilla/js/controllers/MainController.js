a25a4dfe1c103bb84fbb586176364b003c9261cb
김정환 <ej88ej@gmail.com>
2017-12-27 00:21:39 +0900

    [1-vanilla] TabView: 탭 클릭 이벤트 바인딩

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,5 +1,6 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../views/TabView.js'
 
 import SearchModel from '../models/SearchModel.js'
 
@@ -11,7 +12,20 @@ export default {
       .on('@submit', e => this.onSubmit(e.detail.input))
       .on('@reset', e => this.onResetForm())
 
+    TabView.setup(document.querySelector('#tabs'))
+      .on('@change', e => this.onChangeTab(e.detail.tabName))
+
     ResultView.setup(document.querySelector('#search-result'))
+
+    this.selectedTab = '추천 검색어'
+    this.renderView()
+  },
+
+  renderView() {
+    console.log(tag, 'rednerView()')
+    TabView.setActiveTab(this.selectedTab)
+
+    ResultView.hide()
   },
 
   onSubmit(input) {
@@ -32,5 +46,10 @@ export default {
 
   onSearchResult(data) {
     ResultView.render(data)
-  }
+  },
+
+  onChangeTab(tabName) {
+    debugger
+  },
+
 }
