4100e94309e7d1991bd102439094a09159bd5878
김정환 <ej88ej@gmail.com>
2017-12-27 00:57:58 +0900

    [1-vanilla] HistoryView: 출력

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,11 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 import KeywordView from '../views/KeywordView.js'
+import HistoryView from '../views/HistoryView.js'
 
 import SearchModel from '../models/SearchModel.js'
 import KeywordModel from '../models/KeywordModel.js'
+import HistoryModel from '../models/HistoryModel.js'
 
 const tag = '[MainController]'
 
@@ -20,9 +22,12 @@ export default {
     KeywordView.setup(document.querySelector('#search-keyword'))
       .on('@click', e => this.onClickKeyword(e.detail.keyword))
 
+    HistoryView.setup(document.querySelector('#search-history'))
+      .on('@click', e => this.onClickHistory(e.detail.keyword))
+
     ResultView.setup(document.querySelector('#search-result'))
 
-    this.selectedTab = '추천 검색어'
+    this.selectedTab = '최근 검색어'
     this.renderView()
   },
 
@@ -32,8 +37,10 @@ export default {
     
     if (this.selectedTab === '추천 검색어') {
       this.fetchSearchKeyword()
+      HistoryView.hide()
     } else {
-      debugger
+      this.fetchSearchHistory()
+      KeywordView.hide()
     }
 
     ResultView.hide()
@@ -42,6 +49,12 @@ export default {
   fetchSearchKeyword() {
     KeywordModel.list().then(data => {
       KeywordView.render(data)
+    })
+  },
+
+  fetchSearchHistory() {
+    HistoryModel.list().then(data => {
+      HistoryView.render(data)
     })
   },
 
@@ -65,6 +78,7 @@ export default {
   onSearchResult(data) {
     TabView.hide()
     KeywordView.hide()
+    HistoryView.hide()
     ResultView.render(data)
   },
 
@@ -74,5 +88,10 @@ export default {
 
   onClickKeyword(keyword) {
     this.search(keyword)
-  }
+  },
+
+  onClickHistory(keyword) {
+    this.search(keyword)
+  },
+
 }
---
+++ 1-vanilla/js/views/HistoryView.js
@@ -0,0 +1,9 @@
+import KeywordView from './KeywordView.js'
+
+const tag = '[HistoryView]'
+
+const HistoryView = Object.create(KeywordView)
+
+HistoryView.messages.NO_KEYWORDS = '검색 이력이 없습니다'
+
+export default HistoryView
