cf2711a9299566b0d9329bd193846b8034be0d50
김정환 <ej88ej@gmail.com>
2017-12-26 23:42:08 +0900

    [1-vanilla] ResultView: 검색 결과 목록 출력
    This reverts commit 4601ebded7c2a6c071eddcf98c96672243892b79.

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -18,7 +18,17 @@ ResultView.render = function (data = []) {
 }
 
 ResultView.getSearchResultsHtml = function (data) {
-  debugger
+  return data.reduce((html, item) => {
+    html += this.getSearchItemHtml(item)
+    return html
+  }, '<ul>') + '</ul>'
+}
+
+ResultView.getSearchItemHtml = function (item) {
+  return `<li>
+    <img src="${item.image}" />
+    <p>${item.name}</p>
+  </li>`
 }
 
 export default ResultView
