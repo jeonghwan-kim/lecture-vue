dc2e964cee0bdcac1ea52e8e99c8a662c9166cad
김정환 <ej88ej@gmail.com>
2017-12-26 23:18:03 +0900

    [1-vanilla] FormView: 검색어 입력시 x버튼 보임처리

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -9,11 +9,25 @@ FormView.setup = function (el) {
   this.inputEl = el.querySelector('[type=text]')
   this.resetEl = el.querySelector('[type=reset')
   this.showResetBtn(false)
+  this.bindEvents()
   return this
 }
 
 FormView.showResetBtn = function (show = true) {
   this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function () {
+  this.on('submit', e => e.preventDefault())
+  this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+  this.resetEl.addEventListener('click', e => this.onClickReset())
+}
+
+FormView.onKeyup = function (e) {
+  const enter = 13
+  this.showResetBtn(this.inputEl.value.length)
+  if (!this.inputEl.value.length) this.emit('@reset')
+  if (e.keyCode !== enter) return
 }
 
 export default FormView
