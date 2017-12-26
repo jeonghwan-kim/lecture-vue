1eafd324f5eb5833a61eb5947aa736cd1272c516
김정환 <ej88ej@gmail.com>
2017-12-27 01:00:30 +0900

    [1-vanilla] HistoryView: 검색일자, 삭제버튼 추가

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -6,4 +6,16 @@ const HistoryView = Object.create(KeywordView)
 
 HistoryView.messages.NO_KEYWORDS = '검색 이력이 없습니다'
 
+HistoryView.getKeywordsHtml = function (data) {
+  return data.reduce((html, item) => {
+    html += `<li data-keyword="${item.keyword}">
+      ${item.keyword} 
+      <span class="date">${item.date}</span>
+      <button class="btn-remove"></button>
+      </li>`
+    return html
+  }, '<ul class="list">') + "</ul>"
+}
+
+
 export default HistoryView
