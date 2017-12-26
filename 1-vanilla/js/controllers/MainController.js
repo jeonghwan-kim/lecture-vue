36e6f982840c72c34532a8d975fa82f83b66a94d
김정환 <ej88ej@gmail.com>
2017-12-26 23:27:23 +0900

    [1-vanilla] FormView: @reset 이벤트 emit

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -6,9 +6,14 @@ export default {
   init() {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
-  },
-  
+      .on('@reset', e => this.onResetForm())
+},
+
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
   },
+
+  onResetForm() {
+    console.log(tag, 'onResetForm()')
+  }
 }
