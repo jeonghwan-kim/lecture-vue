854f5562b7415192facc761052c32bab4c9ff458
김정환 <ej88ej@gmail.com>
2018-01-04 22:52:47 +0900

    [2-vue] Keyword

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,4 +1,5 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
 new Vue({
   el: '#app',
@@ -7,10 +8,12 @@ new Vue({
     submitted: false,
     tabs: ['추천 검색어', '최근 검색어'],
     selectedTab: '',
+    keywords: [],
     searchResult: []
   },
   created() {
     this.selectedTab = this.tabs[0]
+    this.fetchKeyword()
   },
   methods: {
     onSubmit(e) {
@@ -24,6 +27,15 @@ new Vue({
     },
     onClickTab(tab) {
       this.selectedTab = tab
+    },
+    onClickKeyword(keyword) {
+      this.query = keyword;
+      this.search()
+    },
+    fetchKeyword() {
+      KeywordModel.list().then(data => {
+        this.keywords = data
+      })
     },
     search() {
       SearchModel.list().then(data => {
