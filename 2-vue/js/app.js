2a74e58b6e262c055778c93906e394a9aad3d7c2
김정환 <ej88ej@gmail.com>
2018-01-04 21:44:23 +0900

    [2-vue] result 1

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,17 +1,27 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
   el: '#app',
   data: {
-    query: ''
+    query: '',
+    submitted: false,
+    searchResult: []
   },
   methods: {
     onSubmit(e) {
-      debugger
+      this.search()
     },
     onKeyup(e) {
       if (!this.query.length) this.resetForm()
     },
     onReset(e) {
       this.resetForm()
+    },
+    search() {
+      SearchModel.list().then(data => {
+        this.submitted = true
+        this.searchResult = data
+      })
     },
     resetForm() {
       this.query = ''
