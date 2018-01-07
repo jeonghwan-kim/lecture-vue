7cb41667bd1a9bb1b39a5940c767518d8bc71f91
김정환 <ej88ej@gmail.com>
2018-01-07 15:36:49 +0900

    [4-single-file-component] ResultComponent

---
+++ 4-signel-file-component/src/components/ResultComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="data.length">
+    <ul>
+      <li v-for="item in data">
+        <img v-bind:src="item.image"> {{item.name}}
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    {{query}} 검색어로 찾을수 없습니다 
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data', 'query']
+}
+</script>
