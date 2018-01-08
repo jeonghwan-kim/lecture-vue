5fb274d6c5ce098a5375271b94c828f92a9548bf
김정환 <ej88ej@gmail.com>
2018-01-08 10:33:17 +0900

    tabcomponent 1

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -5,6 +5,7 @@ import HistoryModel from './models/HistoryModel.js'
 import FormComponent from './components/FormComponent.js'
 import ResultComponent from './components/ResultComponent.js'
 import ListComponent from './components/ListComponent.js'
+import TabComponent from './components/TabComponent.js'
 
 new Vue({
   el: '#app',
@@ -20,7 +21,8 @@ new Vue({
   components: {
     'search-form': FormComponent,
     'search-result': ResultComponent,
-    'list': ListComponent
+    'list': ListComponent,
+    'tabs': TabComponent
   },
   created() {
     this.selectedTab = this.tabs[0]
